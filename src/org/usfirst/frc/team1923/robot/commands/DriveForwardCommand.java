4386941f6d817966a441bae90303b4d478ae6012
Christopher Cushman <chriscush765@gmail.com>
2015-01-14 19:54:13 -0500

    Added a command to drive forward at a determined speed

---
+++ src/org/usfirst/frc/team1923/robot/commands/DriveForwardCommand.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team1923.robot.commands;
+
+import org.usfirst.frc.team1923.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveForwardCommand extends Command {
+	
+	private Timer timer = new Timer();
+	
+	private double speed = 0.1;
+
+    public DriveForwardCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer.stop();
+    	timer.reset();
+    	timer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(timer.get() >= 0.5)
+    		Robot.driveTrainSubsystem.manualDrive(0, 0);
+    	else
+    		Robot.driveTrainSubsystem.manualDrive(speed, speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
