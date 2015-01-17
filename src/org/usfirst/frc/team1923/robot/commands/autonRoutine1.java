df977ac261f98bb08f441cc941e7a07781b9ec38
tbruestle <thomas@bruestle.net>
2015-01-17 14:04:18 -0500

    Adding autonSubroutine1

--- src/org/usfirst/frc/team1923/robot/commands/autonRoutine1.java
+++ src/org/usfirst/frc/team1923/robot/commands/autonRoutine1.java
@@ -8,20 +8,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class autonRoutine1 extends CommandGroup {
     
     public  autonRoutine1() {
-    	addSequential(new WaitHalfSecond()); // Replace with Grab Tote
-    	addSequential(new WaitHalfSecond()); // Replace with Elevate Tote
-    	addSequential(new TurnLeft90());
-    	addSequential(new ArcRight180());
-    	addSequential(new TurnLeft90());
-    	addSequential(new MoveForwardOneSecond()); //Replace with Put Tote on Other Tote
 
-    	addSequential(new WaitHalfSecond()); // Replace with Grab Tote
-    	addSequential(new WaitHalfSecond()); // Replace with Elevate Tote
-    	addSequential(new TurnLeft90());
-    	addSequential(new ArcRight180());
-    	addSequential(new TurnLeft90());
-    	addSequential(new MoveForwardOneSecond()); //Replace with Put Tote on Other Tote
-
+    	addSequential(new autonSubroutine1());
+    	addSequential(new autonSubroutine1());
+    	
     	addSequential(new WaitHalfSecond()); // Replace with Grab Tote
     	addSequential(new TurnRight90());
     	addSequential(new MoveForwardTwoSecond());
---
+++ src/org/usfirst/frc/team1923/robot/commands/autonSubroutine1.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1923.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class autonSubroutine1 extends CommandGroup {
+    
+    public  autonSubroutine1() {
+    	addSequential(new WaitHalfSecond()); // Replace with Grab Tote
+    	addSequential(new WaitHalfSecond()); // Replace with Elevate Tote
+    	addSequential(new TurnLeft90());
+    	addSequential(new ArcRight180());
+    	addSequential(new TurnLeft90());
+    	addSequential(new MoveForwardOneSecond()); //Replace with Put Tote on Other Tote
+    }
+}
